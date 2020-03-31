77035df5dab2c8121fbfd3a08c16a442981ef535
Fortich <ajsuarezf@unal.edu.co>
2020-03-31 18:25:29 -0500

    Solved 2d-array

---
+++ 2d-array/main.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int hourglassSum(vector<vector<int>> arr) {
+    int max_sum = INT_MIN;
+    for (int i = 0; i < arr.size() - 2; i++){
+        for (int j = 0; j < arr[0].size() - 2; j++){
+            int sum = arr[i][j] + arr[i][j+1] + arr[i][j+2] + arr[i+1][j+1] + arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2];
+            max_sum = sum > max_sum ? sum : max_sum;
+        }
+    }
+    return max_sum;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
