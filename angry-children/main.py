098aa043e5b16fe4802f78cdeb50760912af73e8
antonio.suarez <antonio.suarez@bold.co>
2022-11-01 17:56:58 -0500

    Solves angry children

---
+++ angry-children/main.py
@@ -0,0 +1,44 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'maxMin' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER k
+#  2. INTEGER_ARRAY arr
+#
+
+def maxMin(k, arr):
+    arr.sort()
+    min_max_min = math.inf
+    for i in range(len(arr) - k + 1):
+        current_max_min = abs(arr[i+k - 1] - arr[i])
+        if current_max_min < min_max_min:
+            min_max_min = current_max_min
+    return min_max_min
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    k = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr_item = int(input().strip())
+        arr.append(arr_item)
+
+    result = maxMin(k, arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
